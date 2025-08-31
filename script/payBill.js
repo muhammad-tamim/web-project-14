59491d39f3844c1782d50ac040f4d8255ef3f5c4
muhammad-tamim <contact2tamim@gmail.com>
2025-08-31 20:45:24 +0600

    feat: add daynamic transaction history form js

--- script/payBill.js
+++ script/payBill.js
@@ -13,6 +13,32 @@ document.getElementById("pay-bill-btn").addEventListener("click", (e) => {
             document.getElementById("main-balance").innerText = subtraction;
             alert(`payment successful ${selectPay}:${payBillAmount} tk`);
 
+            // Transaction history
+            const container = document.getElementById("transaction-history-container");
+
+            const div = document.createElement("div");
+            div.classList.add("flex", "justify-between", "items-center", "bg-white", "shadow-sm", "rounded-xl", "px-4", "py-3");
+
+            const date = new Date();
+            const dataInfo = date.toLocaleDateString();
+            const time = date.toLocaleTimeString();
+
+            div.innerHTML = `
+                    <div class="flex  justify-center items-center gap-3">
+                        <div class="bg-gray-200 rounded-full aspect-square p-3">
+                            <img src="../assets/images/wallet1.png" alt="Add Money">
+                        </div>
+                        <div class="text-gray-700">
+                            <h1 class="font-semibold text-base">${selectPay} Bill: ${payBillAmount}"</h1>
+                            <p class="text-xs">${dataInfo} ${time}</p>
+                        </div>
+                    </div>
+                    <div>
+                        <i class="text-gray-700 pr-2 fa-solid fa-ellipsis-vertical"></i>
+                    </div>
+            `
+
+            container.appendChild(div);
         }
         else {
             alert("Your pin number must be 1234")
